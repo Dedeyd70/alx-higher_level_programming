f240a1a46941f658e42036ea80e50b40c67510c9
Dedeyd70 <dorothyokoto@gmail.com>
2023-07-11 10:52:21 -0700

    assigns a random signed nbr to the variable

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last = abs(number) % 10
-if number < 0:
-    last = -last
-print("Last digit of {} and is {}".format(number, last), end="")
-if last > 5:
-    print("greater than 5")
-elif last == 0:
-    print("0")
+if number >= 0:
+    last = number % 10
+
 else:
-    print("less than 6 and not 0")
+    last = number % -10
+
+print("Last digit of {} is {}".format(number, last), end="")
+
+if last > 5:
+    print(" and is greater than 5")
+elif last == 0:
+    print(" and is 0")
+else:
+    print(" and is less than 6 and not 0")
