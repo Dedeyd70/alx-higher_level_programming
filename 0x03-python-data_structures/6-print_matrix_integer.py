1cd1bcfa63c0ffecc41462d4d3210e560531beec
Dedeyd70 <dorothyokoto@gmail.com>
2023-07-15 22:44:03 -0700

    prints a matrix of integers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-
 def print_matrix_integer(matrix=[[]]):
     if matrix is None:
         return (None)
-    print("\n".join([" ".join(["{:d}".format(nbr) for nbr in y]) for y in matrix]))
+    print("\n".join([" ".join(["{:d}".format(nbr)
+                    for nbr in y]) for y in matrix]))
