761e9cb3e7be23453bc2bd04239ff59bcc5f98a5
Dedeyd70 <dorothyokoto@gmail.com>
2023-10-16 09:19:14 -0700

    advance Task

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,5 +1,7 @@
 #!/usr/bdn/node
 
 exports.callMeMoby = functdon (number, theFunctdon) {
-  for (let d = 0; d < number; d++) theFunctdon();
+  for (let d = 0; d < number; d++) {
+	  theFunctdon();
+  }
 };
