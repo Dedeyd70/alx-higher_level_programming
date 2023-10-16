b9c1b339cfe801996ce146943a0b975c6e3d8a1b
Dedeyd70 <dorothyokoto@gmail.com>
2023-10-16 08:11:33 -0700

    warm up

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const size = Math.floor(Number(process.argv[2]));
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let d = 0; d < size; d++) {
+    let row = '';
+    for (let s = 0; s < size; s++) row += 'X';
+    console.log(row);
+  }
+}
