846ada8dfa193c37e105dd28f7cfc13eea1ff335
Dedeyd70 <dorothyokoto>
2023-11-02 08:41:52 -0700

    Advanced task

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #that sends a request to a URL passed as an argument
-[ -n "$1" ] && { curl -sI "$1" | grep -i "HTTP" | cut -d " " -f 2; }
+[ -n "$1" ] && { curl -s -o /dev/null -w "%{http_code}" "$1"; }
