2046ac9d8dfa2d5a4f26f8a86b6c4dc7e8f096f5
Dedeyd70 <dorothyokoto>
2023-11-02 07:06:05 -0700

    task 3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL and displays all HTTP methods the server
-[ -n "$1" ] && curl -sI "$1" | grep -i "Allow:" | awk '{print $2}'
+curl -Is "$1" | grep "Allow:" | cut -d ":" -f 2 | cur -c 2- | rev | cut -c 2 - | rev
