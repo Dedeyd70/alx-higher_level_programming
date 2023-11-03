63175ffd8733af5e019ad1464288c0c980e66c42
Dedeyd70 <dorothyokoto@gmail.com>
2023-11-03 06:20:34 -0700

    fetching url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
 
     with urllib.request.urlopen(url) as response:
         html = response.read()
-        print(f"-Body Response:")
+        print("Body response:")
         print(f"\t- type: {type(html)}")
         print(f"\t- content: {html}")
-        print(f"\t- ut8 content: {html.decode('utf-8')}")
+        print(f"\t- utf8 content: {html.decode('utf-8')}")
