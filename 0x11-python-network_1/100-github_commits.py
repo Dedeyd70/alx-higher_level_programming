3fd502b97beb4becc755db4949cf9ed895adc7d9
Dedeyd70 <dorothyokoto@gmail.com>
2023-11-03 17:47:47 -0700

    updated

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,7 +5,8 @@ import requests
 
 if __name__ == "__main__":
 
-    url = "https://api.github.com/repos/{}/{}commits".format(sys.argv[2], sys.argv[1])
+    url = "https://api.github.com/repos/{}/{}commits".format
+    (sys.argv[2], sys.argv[1])
     response = requests.get(url)
     commits = response.json()
     try:
