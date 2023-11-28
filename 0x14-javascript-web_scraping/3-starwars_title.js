ffc86e5f62527849ed3c02807da4fab8de202c7b
Dedeyd70 <dorothyokoto@gmail.com>
2023-11-28 06:10:51 -0800

    star wars movie title

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const request = require('request');
+const movieID = process.argv[2];
+const apiURL = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
+
+request.get(apiURL, (err, response, body) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  const movieData = JSON.parse(body);
+  if (movieData.detail === 'Not found') {
+    console.log('Err: Movie with ID ${movieID} not found.');
+    return;
+  }
+  console.log(`${movieData.title}`);
+});
