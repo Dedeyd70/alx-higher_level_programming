283fd1e2eeea891d520592d45a50866ef2d7ab13
Dedeyd70 <dorothyokoto@gmail.com>
2023-12-06 17:15:34 +0000

    No jquery

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Wait for the DOM to be fully loaded before manipulating it
+    var headerElement = document.querySelector('header');
+
+    // Check if the header element is found
+    if (headerElement) {
+      // Update the text color of the header element to red
+      headerElement.style.color = '#FF0000';
+    } else {
+      console.error('Header element not found');
+    }
+  });
