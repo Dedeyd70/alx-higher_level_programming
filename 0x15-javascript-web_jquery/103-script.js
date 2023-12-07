6bf8a2877e83118f6aa53c033336a824fddf6e62
Dedeyd70 <dorothyokoto@gmail.com>
2023-12-07 01:29:36 +0000

    press enter updated

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -3,17 +3,16 @@
       function fetchTranslation() {
         const languageCode = $("#language_code").val();
 
-        // Make an AJAX request to fetch the translation
         $.ajax({
-          url: "https://www.fourtonfish.com/hellosalut/hello/",
+          url: "https://hellosalut.stefanbohacek.dev/?",
           method: "GET",
           data: { lang: languageCode },
           success: function(data) {
-            // Update the content of the <div> with the fetched translation
+
             $("#hello").text(data.hello);
           },
           error: function() {
-            // Handle errors if the request fails
+           
             $("#hello").text("Error: Failed to fetch translation");
           }
         });
@@ -22,8 +21,7 @@
       // Trigger translation on button click
       $("#btn_translate").click(fetchTranslation);
 
-      // Trigger translation on pressing ENTER in the input field
-      $("#language_code").keypress(function(event) {
+           $("#language_code").keypress(function(event) {
         if (event.which === 13) {
 		fetchTranslation();
         }
