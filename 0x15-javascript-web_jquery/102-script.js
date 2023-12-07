301ac08511ed4c3c5aba923cde25a0bd9d61bc6c
Dedeyd70 <dorothyokoto@gmail.com>
2023-12-07 01:21:22 +0000

    say hello to everybody updated

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,19 +1,18 @@
 $(document).ready(function() {
-      $("INPUT#btn_translate").click(function() {
-        // Get the language code entered by the user
-        let languageCode = $("INPUT#language_code").val();
+      $("#btn_translate").click(function() {
+       
+        let languageCode = $("#language_code").val();
 
-        // Make an AJAX request to fetch the translation
         $.ajax({
-          url: "https://www.fourtonfish.com/hellosalut/hello/",
+          url: "https://hellosalut.stefanbohacek.dev/?",
           method: "GET",
           data: { lang: languageCode },
           success: function(data) {
-            // Update the content of the <div> with the fetched translation
-            $("#DIVhello").text(data.hello);
+            
+            $("#hello").text(data.hello);
           },
           error: function() {
-            // Handle errors if the request fails
+           
             $("#hello").text("Error: Failed to fetch translation");
           }
         });
